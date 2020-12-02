03b9fb4b67e35128f4a28396f15bd15f52fdc6e6
sheogorath88 <e.slowioczek@gmail.com>
2020-12-02 20:28:16 +0100

    add method whichBigger

--- src/com/test/Main.java
+++ src/com/test/Main.java
@@ -1,11 +1,30 @@
 package com.test;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	xmas();
+
+//        xmas();
+
+        whichBigger();
+
     }
-    public static void xmas(){
+    private static void whichBigger() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Witaj, podaj proszę dwie liczby");
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        if (a > b) {
+            System.out.println("Pierwsza liczba jest większa");
+        } else if (a < b) {
+            System.out.println("Druga liczba jest większa");
+        } else {
+            System.out.println("Podane liczby są sobie równe");
+        }
+    }
+    public static void xmas() {
         System.out.println("hello, Xmas!");
     }
 }
